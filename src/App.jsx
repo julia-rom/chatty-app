8465d75c86ba185d1cc041552a886942aa4eeb5a
Julia <juliaromanowski@gmail.com>
2019-02-13 23:10:59 +0000

    made server handle incoming messages then send out object to client side

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,12 @@ class App extends Component {
       loading: true,
       currentUser: { name: 'anonymous' },
       message: "",
-      messages: [{
-        type: 'incomingMessage',
-        content: 'I will not be impressed with technology until I can download food.',
-        username: 'Anonymous1',
-        id: 1
-      },
-      {
-        type: 'incomingNotification',
-        content: 'Anonymous1 changed their name to nomnom',
-        username: 'nomnom',
-        id: 2
-      }]
+      messages: [] //msgs coming form server will be stored here
     };
   }
 
-  updateUsername = (name) => {
+  //send username updates to server
+  sendUsername = (name) => {
     // const parsedName = JSON.parse(name.data);
     this.setState({
       currentUser: { name: name },
@@ -36,21 +26,21 @@ class App extends Component {
         username: this.state.currentUser.name
       })
     })
-    this.socket.send(`User ${this.state.currentUser.name} updated their name to ${name}`);
+    this.socket.send(JSON.stringify({ oldUsername: this.state.currentUser.name, newName: name }));
+
   }
 
-  updateMessages = (message) => {
-    // const parsedMsg = JSON.parse(message.data);
-    this.setState({
-      messages: this.state.messages.concat({
-        content: message,
-        id: (this.state.messages.length + 1),
-        type: 'incomingMessage',
-        username: this.state.currentUser.name
-      })
-    })
-    // this.socket.send(JSON.stringify({ message: message, username: this.state.currentUser.name }));
-    this.socket.send(`User ${this.state.currentUser.name} said ${message}`);
+  //sends new msgs to server
+  sendMessages = (message) => {
+    // this.setState({
+    //   messages: this.state.messages.concat({
+    // content: message,
+    // id: (this.state.messages.length + 1),
+    // type: 'incomingMessage',
+    // username: this.state.currentUser.name
+    //   })
+    // })
+    this.socket.send(JSON.stringify({ username: this.state.currentUser.name, message: message }));
   }
 
   componentDidMount() {
@@ -64,19 +54,16 @@ class App extends Component {
       });
 
       // Handle messages using `this.receiveMessage`
-      this.socket.addEventListener("message", this.updateMessages)
+      // this.socket.addEventListener("message", this.sendMessages)
+      this.socket.onmessage = (event) => {
+        const msg = JSON.parse(event.data);
+        console.log(msg)
+        this.sendMessages;
+      }
 
-      // Handle username changes using `this.receiveMessage`
-      this.socket.addEventListener("name change", this.updateUsername)
-
-
-      console.log('Simulating incoming message');
-      // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: 'Michelle', content: 'Hello there!' };
-      const messages = this.state.messages.concat(newMessage)
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages, loading: false })
+      this.setState({ loading: false })
     }, 2000);
   }
 
@@ -89,7 +76,7 @@ class App extends Component {
       return (
         <div>
           < MessageList messages={this.state.messages} />
-          < ChatBar updateUsername={this.updateUsername} updateMessages={this.updateMessages} currentUser={this.state.currentUser} />
+          < ChatBar sendUsername={this.sendUsername} sendMessages={this.sendMessages} currentUser={this.state.currentUser} />
         </div>
       );
     }
